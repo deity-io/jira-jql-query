44d913bf05d63582a655ea0883dce0b9d0671589
Velin Ekupov <velin@deity.io>
2024-07-17 10:51:30 +0200

    set gh output based on jql result

--- index.js
+++ index.js
@@ -1,50 +1,51 @@
-const fs = require('fs')
-const YAML = require('yaml')
-const core = require('@actions/core')
+const fs = require('fs');
+const YAML = require('yaml');
+const core = require('@actions/core');
 
-const configPath = `${process.env.HOME}/jira/config.yml`
-const Action = require('./action')
+const configPath = `${process.env.HOME}/jira/config.yml`;
+const Action = require('./action');
 
 // eslint-disable-next-line import/no-dynamic-require
-const githubEvent = require(process.env.GITHUB_EVENT_PATH)
-const config = YAML.parse(fs.readFileSync(configPath, 'utf8'))
+const githubEvent = require(process.env.GITHUB_EVENT_PATH);
+const config = YAML.parse(fs.readFileSync(configPath, 'utf8'));
 
-async function exec () {
+async function exec() {
   try {
     const result = await new Action({
       githubEvent,
       argv: parseArgs(),
       config,
-    }).execute()
+    }).execute();
 
     if (result) {
-      const extendedConfig = Object.assign({}, config, result)
+      const extendedConfig = Object.assign({}, config, result);
 
-      fs.writeFileSync(configPath, YAML.stringify(extendedConfig))
+      fs.writeFileSync(configPath, YAML.stringify(extendedConfig));
+      core.setOutput('doesIssueExist', result.total > 0);
 
-      return
+      return;
     }
 
-    console.log('Failed to execute the JQL.')
-    process.exit(78)
+    console.log('Failed to execute the JQL.');
+    process.exit(78);
   } catch (error) {
-    console.error(error)
-    process.exit(1)
+    console.error(error);
+    process.exit(1);
   }
 }
 
-function parseArgs () {
-  const jql = core.getInput('jql')
-  let fields = core.getInput('fields')
+function parseArgs() {
+  const jql = core.getInput('jql');
+  let fields = core.getInput('fields');
 
   if (!jql) {
-    throw new Error('Error: please specify a valid jql query.')
+    throw new Error('Error: please specify a valid jql query.');
   }
   if (fields) {
     fields = fields.split(',');
   }
 
-  return { jql, fields }
+  return { jql, fields };
 }
 
-exec()
+exec();
